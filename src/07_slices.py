9f05be50a882f55c8d5e8d32f7d94b556f91d0e7
Leslie Rodriguez <rleslie1015@gmail.com>
2020-07-09 01:49:52 -0500

    finishes 08_comprehensions

--- src/07_slices.py
+++ src/07_slices.py
@@ -12,26 +12,32 @@ Use Python's slice syntax to achieve the following:
 a = [2, 4, 1, 7, 9, 6]
 
 # Output the second element: 4:
-print()
+s1 = slice(1,2)
+print(a[s1])
 
 # Output the second-to-last element: 9
-print()
+s2 = slice(4,5)
+print(a[s2])
 
 # Output the last three elements in the array: [7, 9, 6]
-print()
+s3 = slice(3,6)
+print(a[s3])
 
 # Output the two middle elements in the array: [1, 7]
-print()
+s4 = slice(2,4)
+print(a[s4])
 
 # Output every element except the first one: [4, 1, 7, 9, 6]
-print()
+s5 = slice(1, 6)
+print(a[s5])
 
 # Output every element except the last one: [2, 4, 1, 7, 9]
-print()
+s5 = slice(0,5)
+print(a[s5])
 
 # For string s...
 
 s = "Hello, world!"
-
+s6 = slice(7, 12)
 # Output just the 8th-12th characters: "world"
-print()
+print(s[s6])
--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -10,28 +10,28 @@ for more info regarding list comprehensions.
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
-y = [1, 2, 3, 4, 5]
-print(y)
+y = []
 # long way 
-# squares = []
-# for num in y:
-#     squares.append(num+1)
+# for x in range(5):
+#     y.append(x+1)
+y = [x+1 for x in range(5)]
+print(y)
 
 squares = [num*num for num in y]
 print(squares)
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
-y = []
+cubes = [num**3 for num in y]
 
-print(y)
+print(cubes)
 
 # Write a list comprehension to produce the uppercase version of all the
 # elements in array a. Hint: "foo".upper() is "FOO".
 
 a = ["foo", "bar", "baz"]
 
-y = []
+y = [s.upper() for s in a]
 
 print(y)
 
@@ -41,6 +41,6 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
+y = [num for num in x if int(num) % 2 == 0]
 
 print(y)
